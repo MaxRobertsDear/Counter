78285152a3f2a33ca3bac812cdb4c1a04db3476c
MaxRobertsDear <maxhroberts@gmail.com>
2019-08-10 11:46:16 +0100

    Outsourced actionTypes to sep file This is to ensure that any typos in the actionTypes is caught by an error

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+import * as actionTypes from '../../store/actions'
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
@@ -55,12 +56,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-    onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-    onAddCounter: () => dispatch({type: 'ADD', val: 5}),
-    onSubCounter: () => dispatch({type: 'SUB', val: 5}),
-    onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-    onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElID: id}),
+    onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+    onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+    onAddCounter: () => dispatch({type: actionTypes.ADD, val: 5}),
+    onSubCounter: () => dispatch({type: actionTypes.SUB, val: 5}),
+    onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+    onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElID: id}),
   }
 }
 
